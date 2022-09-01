b147f990b1ebcae76f7dec0957f1183356e69aa5
PreknowledgeWeb <>
2022-09-01 20:38:46 +0800

    Init Commit

--- src/index.js
+++ src/index.js
@@ -39,14 +39,14 @@ term.on('key', function (key, ev) {
     !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
   );
 
-  if (ev.keyCode == 13) {
+  if (ev.keyCode === 13) {
     if(cmd === 'clear')
     {
       term.clear();
     }
     cmd = '';
     term.prompt();
-  } else if (ev.keyCode == 8) {
+  } else if (ev.keyCode === 8) {
     // Do not delete the prompt
     console.log(term.rows);
     if (term.x > 2) {
